5352e10bccdd3cb6dbece134f5669b6f2d4c9e1a
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2021-06-22 04:46:08 +0900

    Update inistall_music_auto_start.sh

--- inistall_music_auto_start.sh
+++ inistall_music_auto_start.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+# wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/main/inistall_music_auto_start.sh && bash inistall_music_auto_start.sh ;
+
+
 mkdir -p ~/.config/lxsession/LXDE-pi ;
 cp /etc/xdg/lxsession/LXDE-pi/autostart ~/.config/lxsession/LXDE-pi/ ;
 echo "omxplayer --no-keys -o local --loop /home/pi/Music/my.mp3 &" >> ~/.config/lxsession/LXDE-pi/autostart ;
