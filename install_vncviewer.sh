9502e32079812850a65c78f3fefa5ff147f4613e
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2021-07-14 15:56:18 +0900

    Update install_vncviewer.sh

--- install_vncviewer.sh
+++ install_vncviewer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-#
+# wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/main/install_vncviewer.sh && bash install_vncviewer.sh ;
 
 sudo apt update ;
 sudo apt install -y snapd ;
