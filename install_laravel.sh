8f418895d882f2253442a42b6ab327ebd5e0a40c
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2021-07-14 16:10:25 +0900

    Update install_laravel.sh

--- install_laravel.sh
+++ install_laravel.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+# wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/edit/main/install_laravel.sh && install_laravel.sh
+
+# see https://raw.githubusercontent.com/mugimugi555/ubuntu/main/install_laravel.sh
+
 cd ;
 sudo echo ;
 wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/install_laravel.sh && bash install_laravel.sh ;
