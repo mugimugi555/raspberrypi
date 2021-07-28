87bd965c936e5cb128570dbbc8127528bfb28e4f
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2021-07-28 14:52:23 +0900

    Update install_edgetpu.sh

--- install_edgetpu.sh
+++ install_edgetpu.sh
@@ -9,7 +9,6 @@ echo "===========================";
 echo "      add repository";
 echo "===========================";
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
-echo "deb https://packages.cloud.google.com/apt coral-cloud-stable main" | sudo tee /etc/apt/sources.list.d/coral-cloud.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 # install app
@@ -26,5 +25,7 @@ sudo apt install -y python3-pycoral ;
 sudo apt install -y python3-tflite-runtime ;
 sudo apt install -y python3-edgetpu ;
 sudo apt install -y edgetpu-examples ;
+
+#echo "deb https://packages.cloud.google.com/apt coral-cloud-stable main" | sudo tee /etc/apt/sources.list.d/coral-cloud.list
 #sudo apt install -y python3-coral-cloudiot ;
 #sudo apt install -y python3-coral-cloudiot ;
