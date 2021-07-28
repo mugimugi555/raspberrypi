0a7df316819552cd4d9dedaa2b0c04d1c4a81ccc
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2021-07-29 07:19:00 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,7 +41,6 @@ sudo raspi-config nonint do_wifi_country JP            ;
 sudo raspi-config nonint do_change_locale ja_JP.UTF-8  ;
 sudo raspi-config nonint do_change_timezone Asia/Tokyo ;
 
-
 #-----------------------------------------------------------------------------------------------------------------------
 # set VNC password for ubuntu client
 #-----------------------------------------------------------------------------------------------------------------------
