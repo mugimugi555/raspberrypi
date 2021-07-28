b51615b496e11287db58d9627a6d36d51a7aefc8
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2021-07-28 15:01:05 +0900

    Update install_edgetpu.sh

--- install_edgetpu.sh
+++ install_edgetpu.sh
@@ -21,15 +21,15 @@ echo "===========================";
 echo "   install edge tpu app";
 echo "===========================";
 sudo apt update ;
-sudo apt install -y edgetpu-compiler ;
-sudo apt install -y libedgetpu1-max ;
-sudo apt install -y libedgetpu1-std ;
-sudo apt install -y libedgetpu-dev ;
-sudo apt install -y gasket-dkmsv ;
-sudo apt install -y python3-pycoral ;
+# sudo apt install -y edgetpu-compiler ; # for raspberry pi not found
+sudo apt install -y libedgetpu1-max    ;
+sudo apt install -y libedgetpu1-std    ;
+sudo apt install -y libedgetpu-dev     ;
+# sudo apt install -y gasket-dkmsv     ; # for raspberry pi not found
+sudo apt install -y python3-pycoral    ;
 sudo apt install -y python3-tflite-runtime ;
-sudo apt install -y python3-edgetpu ;
-sudo apt install -y edgetpu-examples ;
+sudo apt install -y python3-edgetpu    ;
+sudo apt install -y edgetpu-examples   ;
 
 #echo "deb https://packages.cloud.google.com/apt coral-cloud-stable main" | sudo tee /etc/apt/sources.list.d/coral-cloud.list
 #sudo apt install -y python3-coral-cloudiot ;
