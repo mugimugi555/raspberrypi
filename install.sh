047cf7491a4a9a55284cb01b2329d1626ca40a47
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2021-08-04 18:14:54 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -93,8 +93,9 @@ sudo chmod a+rx /usr/local/bin/youtube-dl ;
 #-----------------------------------------------------------------------------------------------------------------------
 # opencv
 #-----------------------------------------------------------------------------------------------------------------------
-sudo pip install --upgrade pip ;
-sudo apt install -y libavutil56 libcairo-gobject2 libgtk-3-0 libqtgui4 libpango-1.0-0 libqtcore4 libavcodec58 libcairo2 libswscale5 libtiff5 libqt4-test libatk1.0-0 libavformat58 libgdk-pixbuf2.0-0 libilmbase23 libjasper1 libopenexr23 libpangocairo-1.0-0 libwebp6 ;
+sudo pip3 install --upgrade pip ;
+sudo apt install -y libavutil56 libcairo-gobject2 libgtk-3-0 libqtgui4 libpango-1.0-0 libqtcore4 libavcodec58 libcairo2 libswscale5 libtiff5 libqt4-test libatk1.0-0 libavformat58 libgdk-pixbuf2.0-0 libilmbase23 libopenexr23 libpangocairo-1.0-0 libwebp6 ;
+# libjasper1 
 sudo pip3 install opencv-python ;
 #sudo pip3 install opencv-python==4.1.0.25
 
