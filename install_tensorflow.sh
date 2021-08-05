f632e17ce17671f15b8ab3b960e266df9ff4a469
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2021-08-05 12:17:57 +0900

    Update install_tensorflow.sh

--- install_tensorflow.sh
+++ install_tensorflow.sh
@@ -2,18 +2,24 @@
 
 # wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/main/install_tensorflow.sh && bash install_tensorflow.sh ;
 
-# for raspberry pi 64 bit
+# for raspberry pi 64 bit and python 3.7
+
+echo "=========================================";
+echo "setup tensorflow 2.5 for python 3.7 cpu arm 64bit";
+echo "if you want other arch , please visit here";
+echo "https://github.com/PINTO0309/Tensorflow-bin";
+echo "=========================================";
+
+# todo check arch
+
+# todo check python3 version
+
 cd ;
 pip cache purge ;
-
 sudo apt install -y libhdf5-dev ;
-
-echo "=========================================";
-echo " download tensorflow 2.5 for 64bit arm";
-echo "=========================================";
-
 wget https://raw.githubusercontent.com/PINTO0309/Tensorflow-bin/main/tensorflow-2.5.0-cp37-none-linux_aarch64_numpy1195_download.sh ;
 bash tensorflow-2.5.0-cp37-none-linux_aarch64_numpy1195_download.sh ;
+
 echo "=========================================";
 echo " install tensorflow 2.5 about 30 minutes";
 echo "=========================================";
