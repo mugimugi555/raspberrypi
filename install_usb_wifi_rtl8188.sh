57bd0ec68743881cc6ed14532c7701880d7de59e
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2021-08-10 08:42:26 +0900

    Update install_usb_wifi_rtl8188.sh

--- install_usb_wifi_rtl8188.sh
+++ install_usb_wifi_rtl8188.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
 
+# wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/main/install_usb_wifi_rtl8188.sh && bash install_usb_wifi_rtl8188.sh
+
 sudo rpi-update dc6dc9bc6692d808fcce5ace9d6209d33d5afbac ;
 
 wget https://raw.githubusercontent.com/UedaTakeyuki/gc_setups/master/rtl8188.setup.sh && rtl8188.setup.sh ;
+
+# sudo reboot now
 
 sudo iwlist wlan0 scan | grep ESSID ;
 
@@ -12,3 +16,5 @@ sudo iwlist wlan0 scan | grep ESSID ;
 #    ssid="SSID"
 #    psk="pass"
 #}
+
+# sudo reboot now
