9610d8518b4956af97f3b8440c677e0f17f418bc
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2021-08-26 10:54:05 +0900

    Create install_printer.sh

---
+++ install_printer.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# wget 
+
+cd ;
+sudo apt install -y libcups2-dev ;
+wget http://jaist.dl.sourceforge.net/project/cups-bjnp/cups-bjnp/2.0.3/cups-bjnp-2.0.3.tar.gz ;
+tar xvf cups-bjnp-2.0.3.tar.gz ;
+cd cups-bjnp-2.0.3 ;
+./configure ;
+make ;
+sudo make install ;
+sudo apt install -y printer-driver-gutenprint ;
