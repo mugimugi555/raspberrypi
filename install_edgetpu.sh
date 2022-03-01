0c753d0b7f1bd1c164ec87a06f1a9d15a2336190
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-01 11:30:56 +0900

    Update install_edgetpu.sh

--- install_edgetpu.sh
+++ install_edgetpu.sh
@@ -1,36 +1,22 @@
 #!/usr/bin/bash
 
-# wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/main/install_edgetpu.sh && bash install_edgetpu.sh ;
+#
 
-# if has error
-# see https://github.com/f0cal/google-coral/issues/95
-# sudo apt update && sudo apt upgrade -y && sudo apt-get dist-upgrade -y ;
-# sudo apt autoremove -y ;
-
-# see https://coral.ai/software/#debian-packages
-
+#=======================================================================================================================
 # add repository
-echo "===========================";
-echo "      add repository";
-echo "===========================";
-echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+#=======================================================================================================================
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list ;
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - ;
 
-# install app
-echo "===========================";
-echo "   install edge tpu app";
-echo "===========================";
+#=======================================================================================================================
+# install edge tpu
+#=======================================================================================================================
 sudo apt update ;
-# sudo apt install -y edgetpu-compiler ; # for raspberry pi not found
-# sudo apt install -y libedgetpu1-max  ;
-sudo apt install -y libedgetpu1-std    ;
-# sudo apt install -y libedgetpu-dev   ;
-# sudo apt install -y gasket-dkmsv     ; # for raspberry pi not found
-sudo apt install -y python3-pycoral    ;
 sudo apt install -y python3-tflite-runtime ;
-sudo apt install -y python3-edgetpu    ;
-sudo apt install -y edgetpu-examples   ;
+#sudo apt install -y libedgetpu1-max ;
+sudo apt install -y libedgetpu1-std ;
+sudo apt install -y libedgetpu-dev ;
+sudo apt install -y python3-pycoral ;
 
-#echo "deb https://packages.cloud.google.com/apt coral-cloud-stable main" | sudo tee /etc/apt/sources.list.d/coral-cloud.list
-#sudo apt install -y python3-coral-cloudiot ;
-#sudo apt install -y python3-coral-cloudiot ;
+#sudo apt install -y python3-edgetpu    ;
+#sudo apt install -y edgetpu-examples   ;
