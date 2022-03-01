cc16b885a879524249beac2577bec6b918c38bd9
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-01 11:34:50 +0900

    Update install_tensorflow.sh

--- install_tensorflow.sh
+++ install_tensorflow.sh
@@ -1,37 +1,21 @@
 #!/usr/bin/bash
 
-# wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/main/install_tensorflow.sh && bash install_tensorflow.sh ;
+#
 
-# for raspberry pi 64 bit and python 3.7
-
-echo "=========================================";
-echo "setup tensorflow 2.5 for python 3.7 cpu arm 64bit";
-echo "if you want other arch , please visit here";
-echo "https://github.com/PINTO0309/Tensorflow-bin";
-echo "=========================================";
-
-# todo check arch
-
-# todo check python3 version
-
+#=======================================================================================================================
+# install tensorflow for 64bit python 3.9
+#=======================================================================================================================
 cd ;
-sudo apt install -y libhdf5-dev ;
-wget https://raw.githubusercontent.com/PINTO0309/Tensorflow-bin/main/tensorflow-2.5.0-cp37-none-linux_aarch64_numpy1195_download.sh ;
-bash tensorflow-2.5.0-cp37-none-linux_aarch64_numpy1195_download.sh ;
-
-echo "=========================================";
-echo " install tensorflow 2.5 about 30 minutes";
-echo "=========================================";
+wget https://github.com/PINTO0309/Tensorflow-bin/releases/download/v2.8.0/tensorflow-2.8.0-cp39-none-linux_aarch64.whl ;
 pip cache purge ;
-python3 -m pip install tensorflow-hub tensorflow-datasets tensorflow-2.5.0-cp37-none-linux_aarch64.whl ;
+python3 -m pip install tensorflow-hub tensorflow-datasets tensorflow-2.8.0-cp39-none-linux_aarch64.whl ;
 
-echo "=========================================";
-echo " check install tensorflow";
-echo "=========================================";
-python3 -c 'import tensorflow as tf; print(tf.__version__)'  # for Python 3
+#=======================================================================================================================
+# pip upgrade
+#=======================================================================================================================
+pip install numpy --upgrade ;
 
-echo "=========================================";
-echo " if has error please input next command";
-echo "=========================================";
-echo "pip uninstall numpy";
-echo "pip install numpy";
+#=======================================================================================================================
+# finished check
+#=======================================================================================================================
+python3 -c 'import tensorflow as tf; print(tf.__version__)' ;
