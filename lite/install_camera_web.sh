f11ada9850bf71863a874ed204f17f7ba8a7eae3
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-04 21:00:56 +0900

    Update install_camera_web.sh

--- lite/install_camera_web.sh
+++ lite/install_camera_web.sh
@@ -23,16 +23,41 @@ make ;
 sudo make install ;
 
 #-----------------------------------------------------------------------------------------------------------------------
-# notice
+# teststart app
+#-----------------------------------------------------------------------------------------------------------------------
+#export LD_LIBRARY_PATH=. ;
+#./mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so" ;
+
+#-----------------------------------------------------------------------------------------------------------------------
+# add service
+#-----------------------------------------------------------------------------------------------------------------------
+MJPG_STREAMER=$(cat<<TEXT
+[Unit]
+Description=mjpgstreamer web streaming server
+After=network.target
+[Service]
+ExecStartPre=
+ExecStart=/usr/local/bin/mjpg_streamer -o "output_http.so -w /usr/local/share/mjpg-streamer/www" -i "input_raspicam.so"
+Type=simple
+User=root
+Group=video
+Restart=always
+[Install]
+WantedBy=multi-user.target
+TEXT
+)
+echo "$MJPG_STREAMER" | sudo tee /etc/systemd/system/mjpgstreamer.service ;
+sudo systemctl enable mjpgstreamer.service ;
+sudo systemctl daemon-reload ;
+sudo systemctl start mjpgstreamer.service ;
+#sudo systemctl stop mjpgstreamer.service ;
+#sudo systemctl disable mjpgstreamer.service ;
+
+#-----------------------------------------------------------------------------------------------------------------------
+# finish
 #-----------------------------------------------------------------------------------------------------------------------
 LOCAL_IPADDRESS=`hostname -I | awk -F" " '{print $1}'` ;
 echo "======================================" ;
 echo "visit =>" ;
 echo "http://$LOCAL_IPADDRESS:8080" ;
 echo "======================================" ;
-
-#-----------------------------------------------------------------------------------------------------------------------
-# start app
-#-----------------------------------------------------------------------------------------------------------------------
-export LD_LIBRARY_PATH=. ;
-./mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so" ;
