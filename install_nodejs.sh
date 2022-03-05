3a705a110457a384d72ba4c8f1f5776cbe4a2e22
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-05 19:56:56 +0900

    Update install_nodejs.sh

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-#
+# wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/main/install_nodejs.sh && bash install_nodejs.sh ;
 
 #-----------------------------------------------------------------------------------------------------------------------
 #
