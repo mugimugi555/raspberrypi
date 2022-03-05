84d691e28dd84309000360b561e1ecea9ee06971
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-05 12:50:59 +0900

    Update install_samba.sh

--- lite/install_samba.sh
+++ lite/install_samba.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-#
+# wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/main/lite/install_samba.sh && bash install_samba.sh ;
 
 #-----------------------------------------------------------------------------------------------------------------------
 # update
