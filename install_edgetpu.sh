f1fe1302732fb6123cdec59761ee9056a6ce71d4
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-05 20:00:02 +0900

    Update install_edgetpu.sh

--- install_edgetpu.sh
+++ install_edgetpu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-#
+# wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/main/install_edgetpu.sh && bash install_edgetpu.sh ;
 
 #-----------------------------------------------------------------------------------------------------------------------
 # add repository
