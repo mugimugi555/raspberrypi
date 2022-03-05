baf2dad7194565ee5872b2dbcbeb55cbe75f7930
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-05 20:08:11 +0900

    Update install_printer.sh

--- install_printer.sh
+++ install_printer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-#
+# wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/main/install_printer.sh && bash install_printer.sh ;
 
 #-----------------------------------------------------------------------------------------------------------------------
 # install library
