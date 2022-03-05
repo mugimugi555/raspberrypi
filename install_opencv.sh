d0179669f11b405d052c8a6661bb4955a169082a
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-05 19:58:06 +0900

    Update install_opencv.sh

--- install_opencv.sh
+++ install_opencv.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
 
-#
+# wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/main/install_opencv.sh && bash install_opencv.sh ;
 
 #-----------------------------------------------------------------------------------------------------------------------
 # install opencv by pip3
 #-----------------------------------------------------------------------------------------------------------------------
 sudo apt update ;
 sudo apt upgrade -y ;
+sudo apt install -y python3-pip ;
 pip3 install opencv-python ;
 
 #-----------------------------------------------------------------------------------------------------------------------
