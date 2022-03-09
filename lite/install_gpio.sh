1bd2625a114590abdda4aeaf536fd1893b0bcb0a
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-10 08:31:11 +0900

    Update install_gpio.sh

--- lite/install_gpio.sh
+++ lite/install_gpio.sh
@@ -11,14 +11,18 @@ sudo apt install -y wiringpi ;
 #-----------------------------------------------------------------------------------------------------------------------
 # manual install
 #-----------------------------------------------------------------------------------------------------------------------
-#sudo apt purge -y wiringpi ;
-#hash -r ;
-#cd ;
-#git clone https://github.com/WiringPi/WiringPi.git ;
-#cd WiringPi cd ;
-#sudo ./build uninstall ;
-#sudo ldconfig ;
-#sudo ./build install ;
+if ! [ -x "$(command -v gpio)" ]; then
+  sudo apt purge -y wiringpi ;
+  sudo apt install -y git ;
+  hash -r ;
+  cd ;
+  git clone https://github.com/WiringPi/WiringPi.git ;
+  cd WiringPi ;
+  sudo ./build ;
+  #sudo ldconfig ;
+  #sudo ./build install ;
+  cd ;
+fi
 
 #-----------------------------------------------------------------------------------------------------------------------
 # check all gpio
