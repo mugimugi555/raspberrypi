9c734a87c011f9211ad27b5de46191323a779a92
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-11 09:52:13 +0900

    Update install_tflite.sh

--- lite/install_tflite.sh
+++ lite/install_tflite.sh
@@ -1,16 +1,27 @@
+#!/usr/bin/bash
 
 exit;
 
-sudo apt update && sudo apt upgrade -y
-sudo apt install cmake
-cd ~
-git clone https://github.com/tensorflow/tensorflow.git tensorflow_src
-mkdir tflite_build
-cd tflite_build
+# wget
+
+#-----------------------------------------------------------------------------------------------------------------------
+#
+#-----------------------------------------------------------------------------------------------------------------------
+sudo apt update ;
+sudo apt upgrade -y ;
+sudo apt install -y cmake ;
+
+#-----------------------------------------------------------------------------------------------------------------------
+#
+#-----------------------------------------------------------------------------------------------------------------------
+cd ~ ;
+git clone https://github.com/tensorflow/tensorflow.git tensorflow_src ;
+mkdir tflite_build ;
+cd tflite_build ;
 sudo cmake ../tensorflow_src/tensorflow/lite \
     -DCMAKE_C_FLAGS="-I/usr/include/python3.9m -I/home/pi/.local/lib/python3.9/site-packages/pybind11/include" \
     -DCMAKE_CXX_FLAGS="-I/usr/include/python3.9m -I/home/pi/.local/lib/python3.9/site-packages/pybind11/include" \
-    -DCMAKE_SHARED_LINKER_FLAGS='-latomic'
+    -DCMAKE_SHARED_LINKER_FLAGS='-latomic' \
     -DCMAKE_SYSTEM_NAME=Linux \
     -DCMAKE_SYSTEM_PROCESSOR=armv6 \
-    -DTFLITE_ENABLE_XNNPACK=OFF
+    -DTFLITE_ENABLE_XNNPACK=OFF ;
