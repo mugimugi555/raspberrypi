942d6fd30e97a95eb2ac9a48a3025b996e8d4e56
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-11 15:14:21 +0900

    Update install_opencv.sh

--- lite/install_opencv.sh
+++ lite/install_opencv.sh
@@ -8,7 +8,21 @@
 sudo apt update ;
 sudo apt upgrade -y ;
 sudo apt install -y python3-pip ;
-sudo apt install -y libatlas-base-dev libopenjp2-7 ;
+sudo apt install -y libatlas-base-dev ;
+sudo apt install -y libopenjp2-7 ;
+sudo apt install -y libavcodec-extra58 ;
+sudo apt install -y libavformat58 ;
+sudo apt install -y libswscale5 ;
+
+# sudo apt install -y libhdf5-103 ;
+# sudo apt install -y libharfbuzz0b ;
+# sudo apt install -y liblapack3 ;
+# sudo apt install -y libwebp6 ;
+# sudo apt install -y libtiff5 ;
+# sudo apt install -y libjasper1 ;
+# sudo apt install -y libilmbase23 ;
+# sudo apt install -y libopenexr23 ;
+
 pip3 install opencv-python-headless ;
 pip3 install opencv-contrib-python-headless ;
 pip3 install numpy matplotlib ;
