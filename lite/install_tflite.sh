c682fa5f5eaef7b30168b4d7fb8ddc96e38fa4c0
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-12 21:48:49 +0900

    Update install_tflite.sh

--- lite/install_tflite.sh
+++ lite/install_tflite.sh
@@ -17,16 +17,17 @@ sudo apt update ;
 sudo apt upgrade -y ;
 sudo apt install -y cmake git ;
 sudo apt install -y python3.9-dev ;
+sudo apt install -y python3-pip ;
 sudo apt install -y python3-numpy ;
-pip3 install pybind11
+pip3 install pybind11 ;
 
 #-----------------------------------------------------------------------------------------------------------------------
 #
 #-----------------------------------------------------------------------------------------------------------------------
-cd ~
-git clone https://github.com/tensorflow/tensorflow.git tensorflow_src
-mkdir tflite_build
-cd tflite_build
+cd ~ ;
+git clone https://github.com/tensorflow/tensorflow.git tensorflow_src ;
+mkdir tflite_build ;
+cd tflite_build ;
 sudo cmake ../tensorflow_src/tensorflow/lite \
     -DCMAKE_C_FLAGS="-I/usr/include/python3.9 -I/home/pi/.local/lib/python3.9/site-packages/pybind11/include" \
     -DCMAKE_CXX_FLAGS="-I/usr/include/python3.9 -I/home/pi/.local/lib/python3.9/site-packages/pybind11/include" \
@@ -34,33 +35,33 @@ sudo cmake ../tensorflow_src/tensorflow/lite \
     -DCMAKE_SYSTEM_NAME=Linux \
     -DCMAKE_SYSTEM_PROCESSOR=armv6 \
     -DTFLITE_ENABLE_XNNPACK=OFF
-sudo cmake --build . --verbose -j 1 -t _pywrap_tensorflow_interpreter_wrapper
+sudo cmake --build . --verbose -j 1 -t _pywrap_tensorflow_interpreter_wrapper ;
 
 #-----------------------------------------------------------------------------------------------------------------------
 #
 #-----------------------------------------------------------------------------------------------------------------------
-cd ~/tensorflow_src/tensorflow/lite/tools/pip_package/
-mkdir gen
-mkdir gen/tflite_pip
-mkdir gen/tflite_pip/python3.9
-mkdir gen/tflite_pip/python3.9/tflite_runtime
-cd gen/tflite_pip/python3.9
-cp -r ~/tensorflow_src/tensorflow/lite/tools/pip_package/debian/ ./
-cp -r ~/tensorflow_src/tensorflow/lite/tools/pip_package/MANIFEST.in ./
-cp -r ~/tensorflow_src/tensorflow/lite/python/interpreter_wrapper/ ./
-cp ~/tensorflow_src/tensorflow/lite/tools/pip_package/setup_with_binary.py ./setup.py
-cp ~/tensorflow_src/tensorflow/lite/python/interpreter.py ./tflite_runtime/
-cp ~/tensorflow_src/tensorflow/lite/python/metrics_interface.py ./tflite_runtime/
-cp ~/tensorflow_src/tensorflow/lite/python/metrics_portable.py ./tflite_runtime/
-cp ~/tflite_build/_pywrap_tensorflow_interpreter_wrapper.so ./tflite_runtime/
-chmod u+w ./tflite_runtime/_pywrap_tensorflow_interpreter_wrapper.so
+cd ~/tensorflow_src/tensorflow/lite/tools/pip_package/ ;
+mkdir gen ;
+mkdir gen/tflite_pip ;
+mkdir gen/tflite_pip/python3.9 ;
+mkdir gen/tflite_pip/python3.9/tflite_runtime ;
+cd gen/tflite_pip/python3.9 ;
+cp -r ~/tensorflow_src/tensorflow/lite/tools/pip_package/debian/ ./ ;
+cp -r ~/tensorflow_src/tensorflow/lite/tools/pip_package/MANIFEST.in ./ ;
+cp -r ~/tensorflow_src/tensorflow/lite/python/interpreter_wrapper/ ./ ;
+cp ~/tensorflow_src/tensorflow/lite/tools/pip_package/setup_with_binary.py ./setup.py ;
+cp ~/tensorflow_src/tensorflow/lite/python/interpreter.py ./tflite_runtime/ ;
+cp ~/tensorflow_src/tensorflow/lite/python/metrics/metrics_interface.py ./tflite_runtime/ ;
+cp ~/tensorflow_src/tensorflow/lite/python/metrics/metrics_portable.py ./tflite_runtime/ ;
+cp ~/tflite_build/_pywrap_tensorflow_interpreter_wrapper.so ./tflite_runtime/ ;
+chmod u+w ./tflite_runtime/_pywrap_tensorflow_interpreter_wrapper.so ;
 
 export PROJECT_NAME=tensorflow_lite ;
 export PACKAGE_VERSION=2.8.0 ;
-touch ./tflite_runtime/__init__.py
-echo "__version__ = '2.8.0'" >> ./tflite_runtime/__init__.py
-echo "__git_version__ = '$(git -C ~/tensorflow_src describe)'" >> ./tflite_runtime/__init__.py
-python3 setup.py bdist --plat-name=linux_armv6l bdist_wheel --plat-name=linux-armv6l
+touch ./tflite_runtime/__init__.py ;
+echo "__version__ = '2.8.0'" >> ./tflite_runtime/__init__.py ;
+echo "__git_version__ = '$(git -C ~/tensorflow_src describe)'" >> ./tflite_runtime/__init__.py ;
+python3 setup.py bdist --plat-name=linux_armv6l bdist_wheel --plat-name=linux-armv6l ;
 
 #-----------------------------------------------------------------------------------------------------------------------
 #
