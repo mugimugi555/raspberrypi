d091bbc80d0e6a346af964aae5e077f0732f15fd
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-12 16:18:43 +0900

    Update install_tflite.sh

--- lite/install_tflite.sh
+++ lite/install_tflite.sh
@@ -16,6 +16,7 @@ sudo /etc/init.d/dphys-swapfile restart ;
 sudo apt update ;
 sudo apt upgrade -y ;
 sudo apt install -y cmake git ;
+sudo apt install -y python3.9-dev ;
 
 #-----------------------------------------------------------------------------------------------------------------------
 #
@@ -25,8 +26,8 @@ git clone https://github.com/tensorflow/tensorflow.git tensorflow_src
 mkdir tflite_build
 cd tflite_build
 sudo cmake ../tensorflow_src/tensorflow/lite \
-    -DCMAKE_C_FLAGS="-I/usr/include/python3.9m -I/home/pi/.local/lib/python3.9/site-packages/pybind11/include" \
-    -DCMAKE_CXX_FLAGS="-I/usr/include/python3.9m -I/home/pi/.local/lib/python3.9/site-packages/pybind11/include" \
+    -DCMAKE_C_FLAGS="-I/usr/include/python3.9 -I/home/pi/.local/lib/python3.9/site-packages/pybind11/include" \
+    -DCMAKE_CXX_FLAGS="-I/usr/include/python3.9 -I/home/pi/.local/lib/python3.9/site-packages/pybind11/include" \
     -DCMAKE_SHARED_LINKER_FLAGS='-latomic'\
     -DCMAKE_SYSTEM_NAME=Linux \
     -DCMAKE_SYSTEM_PROCESSOR=armv6 \
