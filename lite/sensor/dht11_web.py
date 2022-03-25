f4a1db5747b36810716ed39fb0ec9828e079d35e
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-25 12:14:52 +0900

    Create dht11_web.py

---
+++ lite/sensor/dht11_web.py
@@ -0,0 +1,21 @@
+rom bottle import route, run
+from bottle import response
+
+import json
+
+import Adafruit_DHT
+sensor = Adafruit_DHT.DHT11
+
+pin = 14
+
+@route("/")
+def books_list():
+
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    books =  {'humidity': humidity, 'temperature': temperature}
+
+    response.headers['Content-Type']  = 'application/json'
+    response.headers['Cache-Control'] = 'no-cache'
+    return json.dumps(books)
+
+run (host='0.0.0.0', port=8081, debug=True, reloader=True)
