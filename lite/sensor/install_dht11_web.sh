718189b35fb4b2aad05598795badbb8646e681f0
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-25 12:20:11 +0900

    Update install_dht11_web.sh

--- lite/sensor/install_dht11_web.sh
+++ lite/sensor/install_dht11_web.sh
@@ -15,19 +15,36 @@ cd Adafruit_Python_DHT ;
 sudo python3 setup.py install ;
 
 #-----------------------------------------------------------------------------------------------------------------------
-#
+# install python library
 #-----------------------------------------------------------------------------------------------------------------------
-cd ;
 pip3 install bottle ;
-sudo python3 dht11_web.py ;
-
 
 #-----------------------------------------------------------------------------------------------------------------------
 # create python file
 #-----------------------------------------------------------------------------------------------------------------------
 cd ;
 MY_DHT11_WEB=$(cat<<TEXT
+rom bottle import route, run
+from bottle import response
 
+import json
+
+import Adafruit_DHT
+sensor = Adafruit_DHT.DHT11
+
+pin = 14
+
+@route("/")
+def books_list():
+
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    books =  {'humidity': humidity, 'temperature': temperature}
+
+    response.headers['Content-Type']  = 'application/json'
+    response.headers['Cache-Control'] = 'no-cache'
+    return json.dumps(books)
+
+run (host='0.0.0.0', port=8081, debug=True, reloader=True)
 
 TEXT
 )
