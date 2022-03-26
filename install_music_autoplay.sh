c04f3f4ca53e73e69e0d5e026b27b79be4375f33
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-26 18:20:52 +0900

    Update install_music_autoplay.sh

--- install_music_autoplay.sh
+++ install_music_autoplay.sh
@@ -28,15 +28,55 @@ mv Nj9AoKzhe3U.mp3 ~/Music/autoplay.mp3 ;
 #-----------------------------------------------------------------------------------------------------------------------
 # setting auto start command
 #-----------------------------------------------------------------------------------------------------------------------
-mkdir -p ~/.config/lxsession/LXDE-pi ;
-cp /etc/xdg/lxsession/LXDE-pi/autostart ~/.config/lxsession/LXDE-pi/ ;
-echo "mpg123 --loop /home/pi/Music/autoplay.mp3 &" >> ~/.config/lxsession/LXDE-pi/autostart ;
+#mkdir -p ~/.config/lxsession/LXDE-pi ;
+#cp /etc/xdg/lxsession/LXDE-pi/autostart ~/.config/lxsession/LXDE-pi/ ;
+#echo "mpg123 --loop /home/pi/Music/autoplay.mp3 &" >> ~/.config/lxsession/LXDE-pi/autostart ;
 #echo "omxplayer --no-keys -o local --loop /home/pi/Music/autoplay.mp3 &" >> ~/.config/lxsession/LXDE-pi/autostart ;
+
+#-----------------------------------------------------------------------------------------------------------------------
+# setting auto start command
+#-----------------------------------------------------------------------------------------------------------------------
+
+# create os start run file
+OS_START_RUN_SHELL=$(cat<<TEXT
+#!/usr/bin/bash
+play -q -v 0.3 /home/$USER/Music/autoplay.mp3 repeat 99999 &
+TEXT
+)
+echo "$OS_START_RUN_SHELL" > ~/.os_start_run.sh ;
+
+# create os end run file
+touch ~/.os_end_run.sh ;
+
+# add permission
+chmod +x ~/.os_start_run.sh ;
+chmod +x ~/.os_end_run.sh ;
+
+# create service file
+OS_START_SERVICE=$(cat<<TEXT
+[Unit]
+Description=Execute at OS startup and terminates
+After=network.target
+[Service]
+Type=oneshot
+ExecStart=/home/$USER/.os_start_run.sh
+ExecStop=/home/$USER/.os_end_run.sh
+RemainAfterExit=true
+[Install]
+WantedBy=multi-user.target
+TEXT
+)
+echo "$OS_START_SERVICE" | sudo tee /etc/systemd/system/autorun.service ;
+
+# add service
+sudo systemctl daemon-reload ;
+sudo systemctl enable autorun.service ;
 
 #-----------------------------------------------------------------------------------------------------------------------
 # finish
 #-----------------------------------------------------------------------------------------------------------------------
+play -q -v 0.3 /home/pi/Music/autoplay.mp3 repeat 99999 &
 echo "============================================" ;
 echo "if you want stop sound. hit the next command" ;
-echo "              pkill mpg123";
+echo "             sudo pkill play";
 echo "============================================" ;
