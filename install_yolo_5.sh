0d89300f4487f1b44b1c6b11d67deff5e5f79d08
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-04-12 20:37:09 +0900

    Update install_yolo_5.sh

--- install_yolo_5.sh
+++ install_yolo_5.sh
@@ -11,3 +11,20 @@ cd ;
 git clone https://github.com/ultralytics/yolov5.git ;
 cd yolov5 ;
 pip install -U -r requirements.txt ;
+
+#
+python detect.py ;
+
+# streaming camera
+# wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/main/install_camera_rtsp.sh && bash install_camera_rtsp.sh ;
+# python detect.py --source rtsp://127.0.0.1:8554/unicast ;
+
+#
+# python detect.py --source 0 # webcam
+#                           file.jpg
+#                           file.avi
+#                           dir/
+#                           dir/*.jpg
+#                           /dev/video0 # csi camera module not work
+#                           rtsp://127.0.0.1:8554/unicast
+#                           http://127.0.0.1/movie.m3u8
