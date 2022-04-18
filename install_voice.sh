07926e732d68339a76348526843d6d12339f99ad
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-04-18 10:50:57 +0900

    Update install_voice.sh

--- install_voice.sh
+++ install_voice.sh
@@ -2,20 +2,28 @@
 
 # wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/main/install_voice.sh && bash install_voice.sh ;
 
+#-----------------------------------------------------------------------------------------------------------------------
+# install voice app
+#-----------------------------------------------------------------------------------------------------------------------
+sudo echo ;
 sudo apt update ;
 sudo apt install -y open-jtalk ;
 sudo apt install -y open-jtalk-mecab-naist-jdic hts-voice-nitech-jp-atr503-m001 ;
 
 cd ;
 
-# echo
+#-----------------------------------------------------------------------------------------------------------------------
+# voice by echo
+#-----------------------------------------------------------------------------------------------------------------------
 echo "こんにちは" | open_jtalk \
   -x /var/lib/mecab/dic/open-jtalk/naist-jdic \
   -m /usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice \
   -ow hello1.wav ;
 aplay hello1.wav ;
 
-# text
+#-----------------------------------------------------------------------------------------------------------------------
+# voice by text
+#-----------------------------------------------------------------------------------------------------------------------
 cd ;
 echo "今日は天気がいいですね" > hello.txt ;
 open_jtalk \
@@ -25,7 +33,9 @@ open_jtalk \
   hello.txt ;
 aplay hello2.wav ;
 
-# girl
+#-----------------------------------------------------------------------------------------------------------------------
+# girl's voice
+#-----------------------------------------------------------------------------------------------------------------------
 cd ;
 wget https://sourceforge.net/projects/mmdagent/files/MMDAgent_Example/MMDAgent_Example-1.7/MMDAgent_Example-1.7.zip ;
 unzip ./MMDAgent_Example-1.7.zip ;
