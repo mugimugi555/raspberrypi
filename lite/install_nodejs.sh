e27ce2a12b558489f1eaf2c9cab4c973bdfba39b
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-07-21 08:11:46 +0900

    Create install_nodejs.sh

---
+++ lite/install_nodejs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+# wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/main/lite/install_nodejs.sh && bash install_nodejs.sh ;
+
+#-----------------------------------------------------------------------------------------------------------------------
+#
+#-----------------------------------------------------------------------------------------------------------------------
+sudo apt install -y nodejs npm ;
+sudo npm install n -g ;
+sudo n stable ;
+sudo apt purge -y nodejs npm ;
+
+sudo npm install -g yarn ;
+sudo apt autoremove -y ;
+
+node -v ;
+npm  -v ;
+yarn -v ;
