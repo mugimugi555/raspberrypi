9d7618c9f780a353bdf18596cd9b16f0fa21b871
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-07-21 09:38:40 +0900

    Update dht11_web.py

--- lite/sensor/dht11_web.py
+++ lite/sensor/dht11_web.py
@@ -7,22 +7,27 @@ import json
 import datetime
 import Adafruit_DHT
 
-t_delta = datetime.timedelta(hours=9)
-JST = datetime.timezone(t_delta, 'JST')
-now = datetime.datetime.now(JST)
+#
+t_delta  = datetime.timedelta(hours=9)
+JST      = datetime.timezone(t_delta, 'JST')
+now      = datetime.datetime.now(JST)
 datetime = now.strftime('%Y-%m-%d %H:%M:%S')
 
+#
 sensor = Adafruit_DHT.DHT11
-pin = 14
+pin    = 14
 
+#
 @route("/")
 def books_list():
 
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-    books =  {'humidity': humidity, 'temperature': temperature, 'datetime': datetime}
+    #0.81×気温+0.01×相対湿度（0.99×気温-14.3）+46.3
+    comfort = 100 - 0.81 * temperature + 0.01 * humidity * ( 0.99 * temperature - 14.3 ) + 46.3
+    return_json = { 'humidity' : humidity , 'temperature' : temperature , 'datetime' : datetime , 'comfort' : comfort }
 
     response.headers['Content-Type']  = 'application/json'
     response.headers['Cache-Control'] = 'no-cache'
-    return json.dumps(books)
+    return json.dumps( return_json )
 
 run (host='0.0.0.0', port=8081, debug=True, reloader=True)
