6308fdae6b3819f622f4b3e73802b6ec8ee17c76
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-07-22 18:02:30 +0900

    Update install_remove_swapservice.sh

--- lite/install_remove_swapservice.sh
+++ lite/install_remove_swapservice.sh
@@ -10,14 +10,14 @@ sudo rm /var/swap ;
 sudo sync ;
 
 #
-sudo update-rc.d dphys-swapfile disable ;
-sudo rm /etc/init.d/dphys-swapfile ;
-sudo dphys-swapfile disable ;
-sudo dphys-swapfile swapoff ;
-sudo dphys-swapfile uninstall ;
-sudo echo "CONF_SWAPSIZE=0" > /etc/dphys-swapfile ;
-sudo rm /etc/dphys-swapfile ;
-sudo chmod -x /etc/init.d/dphys-swapfile ;
+#sudo update-rc.d dphys-swapfile disable ;
+#sudo rm /etc/init.d/dphys-swapfile ;
+#sudo dphys-swapfile disable ;
+#sudo dphys-swapfile swapoff ;
+#sudo dphys-swapfile uninstall ;
+#sudo echo "CONF_SWAPSIZE=0" > /etc/dphys-swapfile ;
+#sudo rm /etc/dphys-swapfile ;
+#sudo chmod -x /etc/init.d/dphys-swapfile ;
 
 #
 sudo reboot now ;
