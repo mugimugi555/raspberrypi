b429dfeb694c2f70fbf875b6e34922c1bf2b8b22
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-07-22 17:57:35 +0900

    Update install_remove_swapservice.sh

--- lite/install_remove_swapservice.sh
+++ lite/install_remove_swapservice.sh
@@ -2,17 +2,19 @@
 
 # wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/main/lite/install_remove_swapservice.sh && bash install_remove_swapservice.sh ;
 
-sudo sync
-sudo swapoff -a
-sudo apt-get purge -y dphys-swapfile
-sudo rm /var/swap
-sudo sync
+#
+sudo sync ;
+sudo swapoff -a ;
+sudo apt-get purge -y dphys-swapfile ;
+sudo rm /var/swap ;
+sudo sync ;
 
-sudo update-rc.d dphys-swapfile disable
-sudo rm /etc/init.d/dphys-swapfile
-sudo dphys-swapfile disable
-sudo dphys-swapfile swapoff
-sudo dphys-swapfile uninstall
-sudo echo "CONF_SWAPSIZE=0" > /etc/dphys-swapfile
-sudo rm /etc/dphys-swapfile
-sudo chmod -x /etc/init.d/dphys-swapfile
+#
+sudo update-rc.d dphys-swapfile disable ;
+sudo rm /etc/init.d/dphys-swapfile ;
+sudo dphys-swapfile disable ;
+sudo dphys-swapfile swapoff ;
+sudo dphys-swapfile uninstall ;
+sudo echo "CONF_SWAPSIZE=0" > /etc/dphys-swapfile ;
+sudo rm /etc/dphys-swapfile ;
+sudo chmod -x /etc/init.d/dphys-swapfile ;
