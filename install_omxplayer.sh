5f418937c62fa1c447b07e7d8720f7ac244109dc
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-07-22 11:28:05 +0900

    Create install_omxplayer.sh

---
+++ install_omxplayer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+# wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/main/install_omxplayer.sh && bash install_omxplayer.sh ;
+
+#https://www.snowinnovember.info/archives/20220319-raspberry-pi-omxplayer.html
+
+curl -O https://archive.raspberrypi.org/debian/pool/main/o/omxplayer/omxplayer_20190723+gitf543a0d-1+bullseye_armhf.deb ;
+sudo apt install -y ./omxplayer_20190723+gitf543a0d-1+bullseye_armhf.deb ;
+
+cd /usr/lib/arm-linux-gnueabihf ;
+sudo curl -sSfLO 'https://raw.githubusercontent.com/raspberrypi/firmware/master/opt/vc/lib/libbrcmEGL.so'    ;
+sudo curl -sSfLO 'https://raw.githubusercontent.com/raspberrypi/firmware/master/opt/vc/lib/libopenmaxil.so'  ;
+sudo curl -sSfLO 'https://raw.githubusercontent.com/raspberrypi/firmware/master/opt/vc/lib/libbrcmGLESv2.so' ;
+
+omxplayer -o local /usr/share/sounds/alsa/Front_Center.wav ;
