8c3976f732471a8b2b1df9a600a393f801b2e5d4
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-07-22 16:56:16 +0900

    Create install_bgm_mpg123.sh

---
+++ lite/install_bgm_mpg123.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/bash
+
+sudo echo ;
+sudo apt update ;
+sudo apt install -y mpg123 ;
+
+#-----------------------------------------------------------------------------------------------------------------------
+# install youtube dl
+#-----------------------------------------------------------------------------------------------------------------------
+sudo apt install -y ffmpeg ;
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl ;
+sudo chmod a+rx /usr/local/bin/youtube-dl ;
+cd ;
+mkdir ~/Music ;
+youtube-dl --id -x --audio-format mp3 https://www.youtube.com/watch?v=Nj9AoKzhe3U ;
+mv Nj9AoKzhe3U.mp3 ~/Music/autoplay.mp3 ;
+
+#-----------------------------------------------------------------------------------------------------------------------
+# setting auto start command
+#-----------------------------------------------------------------------------------------------------------------------
+
+# create os start run file
+OS_START_RUN_SHELL=$(cat<<TEXT
+#!/usr/bin/bash
+mpg123 -Z /home/$USER/Music/autoplay.mp3 &
+TEXT
+)
+echo "$OS_START_RUN_SHELL" > ~/.os_start_run.sh ;
+
+# create os end run file
+touch ~/.os_end_run.sh ;
+
+# add permission
+chmod +x ~/.os_start_run.sh ;
+chmod +x ~/.os_end_run.sh ;
+
+# create service file
+OS_START_SERVICE=$(cat<<TEXT
+[Unit]
+Description=Execute at OS startup and terminates
+After=network.target
+[Service]
+Type=oneshot
+ExecStartPre=
+ExecStart=/home/$USER/.os_start_run.sh
+ExecStop=/home/$USER/.os_end_run.sh
+RemainAfterExit=true
+[Install]
+WantedBy=multi-user.target
+TEXT
+)
+echo "$OS_START_SERVICE" | sudo tee /etc/systemd/system/autorun.service ;
+
+# add service
+sudo systemctl daemon-reload ;
+sudo systemctl enable autorun.service ;
+sudo systemctl start autorun.service ;
+
+#-----------------------------------------------------------------------------------------------------------------------
+# finish
+#-----------------------------------------------------------------------------------------------------------------------
+echo "============================================" ;
+echo "sudo systemctl start autorun.service" ;
+echo "sudo systemctl stop autorun.service" ;
+echo "============================================" ;
