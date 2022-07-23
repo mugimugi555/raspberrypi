4f8da9980318b06eb90ccf66ceb0bc4108e7f6c0
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-07-23 11:58:05 +0900

    Update install.sh

--- dietpi/install.sh
+++ dietpi/install.sh
@@ -3,3 +3,6 @@ sudo apt upgrade -y ;
 sudo apt install -y avahi-daemon git make python3 python3-dev python-is-python3 ;
 
 # sudo apt install -y avahi-daemon ;
+
+# https://www.tomoyan.net/hardware/raspberry_pi/dietpi/zram_swap
+# sudo /boot/dietpi/func/dietpi-set_swapfile 0
