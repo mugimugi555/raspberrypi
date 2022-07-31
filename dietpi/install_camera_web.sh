4fa399c2f1c4351bd4dd31871f999982625289f6
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-08-01 06:26:43 +0900

    Update install_camera_web.sh

--- dietpi/install_camera_web.sh
+++ dietpi/install_camera_web.sh
@@ -3,14 +3,19 @@
 # wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/main/dietpi/install_camera_web.sh && bash install_camera_web.sh ;
 
 #-----------------------------------------------------------------------------------------------------------------------
-# enable legacy camera ( 0 is on )
+# install mjpg-streamer 
 #-----------------------------------------------------------------------------------------------------------------------
 
 sudo dietpi-config
 # enable camera DietPi-Config/Display Options/
 
-# https://raw.githubusercontent.com/MichaIng/DietPi/dev/dietpi/dietpi-software
 dietpi-software install 137 ; # mjpg-stream
+
+#-----------------------------------------------------------------------------------------------------------------------
+# setting for camera v1.3
+#-----------------------------------------------------------------------------------------------------------------------
+# nano /etc/systemd/system/mjpg-streamer.service
+# replace lib_raspicam.so -> lib_uvc.so
 
 #-----------------------------------------------------------------------------------------------------------------------
 # finish
