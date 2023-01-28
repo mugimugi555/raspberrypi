b0f29356a389acdbf8088a783ac77704d5928cb4
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2023-01-28 18:53:26 +0900

    Update install_edgetpu.sh

--- install_edgetpu.sh
+++ install_edgetpu.sh
@@ -7,7 +7,6 @@
 #-----------------------------------------------------------------------------------------------------------------------
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - ;
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list ;
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - ;
 
 #-----------------------------------------------------------------------------------------------------------------------
 # install edge tpu
