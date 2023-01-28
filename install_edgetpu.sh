4a76b6910e08b8d445e10089dcbe509958ff9013
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2023-01-28 18:48:56 +0900

    Update install_edgetpu.sh

--- install_edgetpu.sh
+++ install_edgetpu.sh
@@ -5,6 +5,7 @@
 #-----------------------------------------------------------------------------------------------------------------------
 # add repository
 #-----------------------------------------------------------------------------------------------------------------------
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - ;
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list ;
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - ;
 
