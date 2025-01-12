8f82395daec3ee44111b9f30b270f7493d051d11
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2025-01-13 01:21:36 +0900

    Create install_emacs.sh

---
+++ dietpi/install_emacs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# .emacs.d/init.el ファイルパス
+INIT_EL="$HOME/.emacs.d/init.el"
+
+# 追記する内容
+SETTINGS='
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+'
+
+# ファイルが存在しない場合、新規作成する
+if [ ! -f "$INIT_EL" ]; then
+  mkdir -p "$(dirname "$INIT_EL")"
+  touch "$INIT_EL"
+fi
+
+# 追記する内容をファイルに追加
+echo "$SETTINGS" >> "$INIT_EL"
+
+echo "設定を $INIT_EL に追記しました。"
