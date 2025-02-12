e953b1a1d7c835672ee8b46d72e3ad6a39a56480
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2025-02-12 16:19:01 +0900

    Update install_nodejs.sh

--- dietpi/install_nodejs.sh
+++ dietpi/install_nodejs.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-echo "Updating package list..."
-sudo apt update
-
-echo "Installing required dependencies..."
-sudo apt install -y curl software-properties-common
-
-echo "Adding NodeSource repository for latest Node.js..."
-curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
-
-echo "Installing latest Node.js and npm..."
-sudo apt install -y nodejs
+sudo /boot/dietpi/dietpi-software install 9
 
 echo "Checking installed versions..."
 node -v
