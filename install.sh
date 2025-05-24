5f08eb4a71654f3d67b8945596fb38484dfed0ed
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2025-05-24 12:10:35 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,95 +1,104 @@
 #!/bin/bash
 
-# wget https://raw.githubusercontent.com/mugimugi555/raspberrypi/main/install.sh && bash install.sh ;
+# 初期セットアップスクリプト for Raspberry Pi 4 🛠️🍓
+# https://github.com/mugimugi555/raspberrypi/blob/main/install.sh で実行可能
 
-#-----------------------------------------------------------------------------------------------------------------------
-# sudo time out
-#-----------------------------------------------------------------------------------------------------------------------
-echo 'Defaults timestamp_timeout = 1200' | sudo EDITOR='tee -a' visudo ;
+set -e  # エラー時に即停止
 
-#-----------------------------------------------------------------------------------------------------------------------
-# wall paper
-#-----------------------------------------------------------------------------------------------------------------------
-wget http://gahag.net/img/201602/11s/gahag-0055029460-1.jpg -O /home/$USER/Pictures/1.jpg ;
-pcmanfm -w /home/$USER/Pictures/1.jpg ;
-feh --bg-scale /home/$USER/Pictures/1.jpg ;
+#---------------------------------------------
+# sudo timeout の延長 ⏳
+#---------------------------------------------
+echo "🛡️ sudo タイムアウトを延長中..."
+echo 'Defaults timestamp_timeout = 1200' | sudo EDITOR='tee -a' visudo
 
-#-----------------------------------------------------------------------------------------------------------------------
-# config
-#-----------------------------------------------------------------------------------------------------------------------
-sudo raspi-config nonint do_change_pass                ;
-sudo raspi-config nonint do_vnc 0                      ;
-sudo raspi-config nonint do_camera 0                   ;
-sudo raspi-config nonint do_legacy 0                   ;
-sudo raspi-config nonint do_i2c 0                      ;
-sudo raspi-config nonint do_ssh 0                      ;
-sudo raspi-config nonint do_spi 0                      ;
-sudo raspi-config nonint do_wifi_country JP            ;
-sudo raspi-config nonint do_change_locale ja_JP.UTF-8  ;
-sudo raspi-config nonint do_change_timezone Asia/Tokyo ;
+#---------------------------------------------
+# 壁紙の設定 🖼️
+#---------------------------------------------
+echo "🖼️ 壁紙を設定中..."
+WALLPAPER_DIR="$HOME/Pictures"
+WALLPAPER_IMG="$WALLPAPER_DIR/1.jpg"
+mkdir -p "$WALLPAPER_DIR"
+wget -q http://gahag.net/img/201602/11s/gahag-0055029460-1.jpg -O "$WALLPAPER_IMG"
+pcmanfm -w "$WALLPAPER_IMG" || true
+feh --bg-scale "$WALLPAPER_IMG" || true
 
-#-----------------------------------------------------------------------------------------------------------------------
-# disable screen blank
-#-----------------------------------------------------------------------------------------------------------------------
-sudo mkdir -p /etc/X11/xorg.conf.d/ ;
-sudo cp /usr/share/raspi-config/10-blanking.conf /etc/X11/xorg.conf.d/ ;
+#---------------------------------------------
+# Raspberry Pi 設定 🔧
+#---------------------------------------------
+echo "⚙️ Raspberry Pi 基本設定中..."
+sudo raspi-config nonint do_change_pass
+sudo raspi-config nonint do_vnc 0
+sudo raspi-config nonint do_camera 0
+sudo raspi-config nonint do_legacy 0
+sudo raspi-config nonint do_i2c 0
+sudo raspi-config nonint do_ssh 0
+sudo raspi-config nonint do_spi 0
+sudo raspi-config nonint do_wifi_country JP
+sudo raspi-config nonint do_change_locale ja_JP.UTF-8
+sudo raspi-config nonint do_change_timezone Asia/Tokyo
 
-#-----------------------------------------------------------------------------------------------------------------------
-# locale
-#-----------------------------------------------------------------------------------------------------------------------
-export LANGUAGE=ja_JP.utf8 ;
-export LANG=ja_JP.utf8 ;
-export LC_ALL=ja_JP.utf8 ;
-sudo locale-gen ja_JP.UTF-8 ;
-sudo /usr/sbin/update-locale LANG=ja_JP.UTF-8 ;
-LC_ALL=C xdg-user-dirs-update --force ;
+#---------------------------------------------
+# 画面スリープ無効化 💤
+#---------------------------------------------
+echo "💡 スクリーンブランキングを無効化中..."
+sudo install -D /usr/share/raspi-config/10-blanking.conf /etc/X11/xorg.conf.d/10-blanking.conf
 
-#-----------------------------------------------------------------------------------------------------------------------
-# swap
-#-----------------------------------------------------------------------------------------------------------------------
-sudo echo "CONF_SWAPSIZE=4096" | sudo tee /etc/dphys-swapfile ;
-sudo /etc/init.d/dphys-swapfile restart ;
+#---------------------------------------------
+# ロケール設定 🌐
+#---------------------------------------------
+echo "🌐 ロケールを日本語に設定中..."
+export LANGUAGE=ja_JP.utf8
+export LANG=ja_JP.utf8
+export LC_ALL=ja_JP.utf8
+sudo locale-gen ja_JP.UTF-8
+sudo update-locale LANG=ja_JP.UTF-8
+LC_ALL=C xdg-user-dirs-update --force
 
-#-----------------------------------------------------------------------------------------------------------------------
-# software
-#-----------------------------------------------------------------------------------------------------------------------
-echo "samba-common samba-common/workgroup string  WORKGROUP" | sudo debconf-set-selections ;
-echo "samba-common samba-common/dhcp boolean true"           | sudo debconf-set-selections ;
-echo "samba-common samba-common/do_debconf boolean true"     | sudo debconf-set-selections ;
-sudo apt update ;
-sudo apt upgrade -y ;
-sudo apt install -y                       \
-  emacs-nox htop curl git axel            \
-  samba net-tools exfat-fuse              \
+#---------------------------------------------
+# swapサイズ拡張 🧠
+#---------------------------------------------
+echo "🧠 スワップサイズを 4096MB に設定中..."
+echo "CONF_SWAPSIZE=4096" | sudo tee /etc/dphys-swapfile
+sudo /etc/init.d/dphys-swapfile restart
+
+#---------------------------------------------
+# 必要なソフトウェアのインストール 📦
+#---------------------------------------------
+echo "📦 必要なパッケージをインストール中..."
+echo "samba-common samba-common/workgroup string WORKGROUP" | sudo debconf-set-selections
+echo "samba-common samba-common/dhcp boolean true"         | sudo debconf-set-selections
+echo "samba-common samba-common/do_debconf boolean true"   | sudo debconf-set-selections
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y \
+  emacs-nox htop curl git axel \
+  samba net-tools exfat-fuse exfatprogs \
   ffmpeg ibus-mozc imagemagick mpg321 vlc \
-  lame unar lshw                          \
-  mosquitto-clients ;
-sudo apt install -y exfat-utils ;
-sudo apt autoremove -y ;
+  lame unar lshw mosquitto-clients
+sudo apt autoremove -y
 
-#-----------------------------------------------------------------------------------------------------------------------
-# snapd
-#-----------------------------------------------------------------------------------------------------------------------
-sudo apt install -y snapd ;
+# Snapdとアプリのインストール 📦
+echo "📥 snapd とアプリをインストール中..."
+sudo apt install -y snapd
 sudo snap install --classic gimp
 sudo snap install --edge yt-dlp
 
-#-----------------------------------------------------------------------------------------------------------------------
-# caps2ctrl
-#-----------------------------------------------------------------------------------------------------------------------
-CAPS2CTRL=$(cat<<TEXT
+#---------------------------------------------
+# CapsLockをCtrlに割当て ⌨️
+#---------------------------------------------
+echo "⌨️ CapsLock を Ctrl に再マップ中..."
+cat << EOF | sudo tee /etc/default/keyboard
 BACKSPACE="guess"
 XKBMODEL="pc105"
 XKBLAYOUT="jp"
 XKBVARIANT=""
 XKBOPTIONS="ctrl:nocaps"
-TEXT
-)
-echo "$CAPS2CTRL" | sudo tee /etc/default/keyboard ;
+EOF
 
-MYKEYBOARD=$(cat<<TEXT
+# Mozc設定（XMLテンプレート）
+echo "📝 Mozc のエンジン設定中..."
+cat << EOF | sudo tee /usr/share/ibus/component/mozc.xml
 <component>
-  <version>2.23.2815.102+dfsg-8ubuntu1</version>
   <name>com.google.IBus.Mozc</name>
   <license>New BSD</license>
   <exec>/usr/lib/ibus-mozc/ibus-engine-mozc --ibus</exec>
@@ -97,41 +106,39 @@ MYKEYBOARD=$(cat<<TEXT
   <author>Google Inc.</author>
   <homepage>https://github.com/google/mozc</homepage>
   <description>Mozc Component</description>
-<engines>
-<engine>
-  <description>Mozc (Japanese Input Method)</description>
-  <language>ja</language>
-  <symbol>&#x3042;</symbol>
-  <rank>80</rank>
-  <icon_prop_key>InputMode</icon_prop_key>
-  <icon>/usr/share/ibus-mozc/product_icon.png</icon>
-  <setup>/usr/lib/mozc/mozc_tool --mode=config_dialog</setup>
-  <layout>jp</layout>
-  <name>mozc-jp</name>
-  <longname>Mozc</longname>
-</engine>
-</engines>
+  <engines>
+    <engine>
+      <description>Mozc (Japanese Input Method)</description>
+      <language>ja</language>
+      <symbol>&#x3042;</symbol>
+      <rank>80</rank>
+      <icon_prop_key>InputMode</icon_prop_key>
+      <icon>/usr/share/ibus-mozc/product_icon.png</icon>
+      <setup>/usr/lib/mozc/mozc_tool --mode=config_dialog</setup>
+      <layout>jp</layout>
+      <name>mozc-jp</name>
+      <longname>Mozc</longname>
+    </engine>
+  </engines>
 </component>
-TEXT
-)
-echo "$MYKEYBOARD" | sudo tee /usr/share/ibus/component/mozc.xml ;
+EOF
 
-#-----------------------------------------------------------------------------------------------------------------------
-# alias
-#-----------------------------------------------------------------------------------------------------------------------
-MYALIAS=$(cat<<TEXT
+#---------------------------------------------
+# alias設定 🧾
+#---------------------------------------------
+echo "🧾 .bashrc にエイリアスを追加中..."
+cat << EOF >> ~/.bashrc
 
 # myalias
 alias a="axel -a -n 10"
 alias u='unar'
 alias up='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y'
+EOF
 
-TEXT
-)
-echo "$MYALIAS" >> ~/.bashrc ;
-source ~/.bashrc ;
+source ~/.bashrc
 
-#-----------------------------------------------------------------------------------------------------------------------
-# reboot
-#-----------------------------------------------------------------------------------------------------------------------
-sudo reboot now ;
+#---------------------------------------------
+# 再起動 🔁
+#---------------------------------------------
+echo "🔁 システムを再起動します..."
+sudo reboot now
